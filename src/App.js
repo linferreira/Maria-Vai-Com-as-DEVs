08770007ea86945e190a6512ff39fa704219c95e
Lindsay Ferreira <eson@iouu.com.br>
2020-08-18 20:58:30 -0300

    criar estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Header, Title, Paragraph, Serasa, Circle, Footer, Box, BoxContainer } from "./styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <Header>
+        <Title>Titulo</Title>
+      </Header>
+
+      <Paragraph>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm standing here on the parking lot of- This is uh, this is heavy
+          duty, Doc, this is great. Uh, does it run on regular unleaded
+          gasoline? You got a permit for that? Your, your right. whoa, whoa Doc,
+          stuck here, I can't be stuck here, I got a life in 1985. I got a girl.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non
+          nibh vulputate tempus sit fringilla egestas. Neque amet sit est quis
+          donec commodo. Mi lacus tempor ac eu lorem eu augue pharetra id. Quam
+          scelerisque donec sed elementum pharetra. Sagittis, pellentesque
+          tellus massa sed sit eget tellus. Ullamcorper fringilla nulla sed
+          faucibus. Malesuada vel, arcu ultrices ultrices. Lectus tellus
+          dictumst mattis amet, iaculis dui quam lacus sed. Diam commodo
+          ultrices massa libero ac, amet sodales.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Paragraph>
+
+      <Serasa>
+        <Circle />
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non
+          nibh vulputate tempus sit fringilla egestas. Neque amet sit est quis
+          donec commodo. Mi lacus tempor ac eu lorem eu augue pharetra id. Quam
+          scelerisque donec sed elementum pharetra
+        </p>
+      </Serasa>
+
+      <BoxContainer>
+        <Box />
+        <Box />
+
+        <Box/>
+      </BoxContainer>
+
+      <Footer />
+    </>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #c4c4c4;
+  padding: 80px 500px;
+`;
+
+export const Title = styled.h1``;
+
+export const Paragraph = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 200px;
+  height: 300px;
+`;
+
+export const Serasa = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 59px 150px 34px;
+  height: 150px;
+  background: #c4c4c4;
+`;
+
+export const Circle = styled.span`
+  background: #fff;
+  height: 100px;
+  width: 150px;
+  margin-right: 148px;
+`;
+
+export const Footer = styled.div`
+  background: #c4c4c4;
+  height: 50px;
+`;
+
+export const BoxContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 150px 130px;
+`;
+
+export const Box = styled.div`
+  background: #c4c4c4;
+  height: 150px;
+  width: 250px;
+`;
